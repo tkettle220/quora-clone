ed617e99982ef5fb1ebd053465a937072186ec6b
Thomas Kilmer <tkilmer95@gmail.com>
2017-09-20 14:58:34 -0700

    add mailer config options in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,6 +64,18 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "quora_#{Rails.env}"
   config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = { host: 'https://quera-fullstack-project.herokuapp.com' }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      :enable_starttls_auto => true,
+      :address => "smtp.gmail.com",
+      :port => 587,
+      :domain => "https://quera-fullstack-project.herokuapp.com",
+      :authentication => :login,
+      :user_name => "quera.user.auth@gmail.com",
+      :password => "go_fullstack_go"
+    }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
